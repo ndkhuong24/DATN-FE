7820008c79e907dee510ea9366a1b7f31e1bba24
ndkhuong24 <kn134646@gmail.com>
2024-05-27 22:08:06 +0700

    cập nhật đế giày

--- font-end-admin/src/app/component/degiay/de-giay-action/de-giay-action.component.ts
+++ font-end-admin/src/app/component/degiay/de-giay-action/de-giay-action.component.ts
@@ -1,20 +1,19 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
-import {SuaDeGiayComponent} from '../sua-de-giay/sua-de-giay.component';
-import {MatDialog} from '@angular/material/dialog';
-import {SoleService} from '../../../service/sole.service';
-import {DegiayComponent} from '../degiay.component';
-import {ICellRendererAngularComp} from 'ag-grid-angular';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { SuaDeGiayComponent } from '../sua-de-giay/sua-de-giay.component';
+import { MatDialog } from '@angular/material/dialog';
+import { SoleService } from '../../../service/sole.service';
+import { DegiayComponent } from '../degiay.component';
+import { ICellRendererAngularComp } from 'ag-grid-angular';
 import Swal from 'sweetalert2';
 @Component({
   selector: 'app-de-giay-action',
   templateUrl: './de-giay-action.component.html',
-  styleUrls: ['./de-giay-action.component.css']
+  styleUrls: ['./de-giay-action.component.css'],
 })
 export class DeGiayActionComponent implements ICellRendererAngularComp, OnInit {
-
   params: any;
   rowData = [];
-  agInit(params: any){
+  agInit(params: any) {
     this.params = params.data;
   }
 
@@ -22,16 +21,18 @@ export class DeGiayActionComponent implements ICellRendererAngularComp, OnInit {
     return false;
   }
 
-  constructor(private matdialog: MatDialog,
-              private slsv: SoleService,
-              private cdr: ChangeDetectorRef,
-              private degiayComponent: DegiayComponent) { }
+  constructor(
+    private matdialog: MatDialog,
+    private slsv: SoleService,
+    private cdr: ChangeDetectorRef,
+    private degiayComponent: DegiayComponent
+  ) {}
 
   ngOnInit(): void {
     this.getAllSole();
   }
-  getAllSole(){
-    this.slsv.getAllSole().subscribe(result => {
+  getAllSole() {
+    this.slsv.getAllSole().subscribe((result) => {
       this.rowData = result;
     });
   }
@@ -39,9 +40,9 @@ export class DeGiayActionComponent implements ICellRendererAngularComp, OnInit {
     const dialogref = this.matdialog.open(SuaDeGiayComponent, {
       width: '65vh',
       height: '75vh',
-      data: this.params
+      data: this.params,
     });
-    dialogref.afterClosed().subscribe(result => {
+    dialogref.afterClosed().subscribe((result) => {
       console.log(result);
       if (result === 'saveSole') {
         this.degiayComponent.ngOnInit();
@@ -57,22 +58,17 @@ export class DeGiayActionComponent implements ICellRendererAngularComp, OnInit {
       showCancelButton: true,
       confirmButtonColor: '#3085d6',
       cancelButtonColor: '#d33',
-      confirmButtonText: 'Xóa!'
+      confirmButtonText: 'Xóa!',
     }).then((result) => {
       if (result.isConfirmed) {
-    sole = this.params.id;
-    console.log(sole);
-    this.slsv.DeleteSole(sole).subscribe(() => {
-      this.degiayComponent.ngOnInit();
-      this.cdr.detectChanges();
-    });
-    Swal.fire(
-          'Xóa!',
-          'Xóa thành công',
-          'success'
-    );
+        sole = this.params.id;
+        console.log(sole);
+        this.slsv.DeleteSole(sole).subscribe(() => {
+          this.degiayComponent.ngOnInit();
+          this.cdr.detectChanges();
+        });
+        Swal.fire('Xóa!', 'Xóa thành công', 'success');
       }
     });
   }
-
 }
